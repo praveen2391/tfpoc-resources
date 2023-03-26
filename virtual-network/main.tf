99c744195599071e97b8f7f1bab2e89a3e793c2e
Praveenkumar Narasimman <praveenkumar.narasimman@atos.net>
2023-03-26 21:39:11 +0530

    vnet update

--- virtual-network/main.tf
+++ virtual-network/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_virtual_network" "virtual_network" {
   resource_group_name = var.rg-name
   /* location            = data.azurerm_resource_group.resource_group.location
   resource_group_name = data.azurerm_resource_group.resource_group.name */
-  address_space       = var.adddress-space
+  address_space       = var.address-space
   dns_servers         = var.dns-servers
 
 
