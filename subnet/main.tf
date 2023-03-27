85cfeccc8669f779ea077ae4dccce04d4d60d821
Praveenkumar Narasimman <praveenkumar.narasimman@atos.net>
2023-03-27 09:15:35 +0530

    subnet update

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet-name
-  resource_group_name  = azurerm_resource_group.resource_group.name
-  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  resource_group_name  = var.rg-name
+  virtual_network_name = var.vnet-name
   address_prefixes     = var.address-prefixes
   
 }
--- subnet/variables.tf
+++ subnet/variables.tf
@@ -6,3 +6,13 @@ variable "subnet-name" {
 variable "address-prefixes" {
   default = ["10.0.1.0/24"]
 }
+
+variable "rg-name" {
+  type = string
+  default = "tfpoc-rg"
+}
+
+variable "vnet-name" {
+  type = string
+  default = "tfpoc-vnet"
+}
